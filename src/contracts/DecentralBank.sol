c5de7239be978af0e5de7a0a06955040556c2b05
Steven Cao <anhhung_so11@yahoo.com>
2023-06-17 11:17:52 +0700

    update

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,5 +1,21 @@
 pragma solidity ^0.5.0;
+import './RWD.sol';
+import './Tether.sol';
 contract DecentralBank {
     string public name = 'Decentral Bank';
     address public owner;
+    Tether public tether;
+    RWD public rwd;
+    mapping(address => uint) public stakingBalance;
+    constructor(RWD _rwd, Tether _tether) public {
+        rwd = _rwd;
+        tether = _tether;
+    }
+    // staking function
+    function depositTokens(uint _amount) public {
+        // Transfer tether tokens to this contract address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+        // Update Staking Balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+    }
 }
