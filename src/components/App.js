5b5836ba19829c677d1de6f1deeae2c7af08d5e2
Steven Cao <anhhung_so11@yahoo.com>
2023-06-18 12:01:39 +0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Web3 from "web3";
 import Tether from "./../truffle_abis/Tether.json";
+import RWD from "./../truffle_abis/RWD.json";
+import DecentralBank from "./../truffle_abis/DecentralBank.json";
 const App = () => {
   const [objectInformation, setObjectInformation] = React.useState({
     tether: {},
@@ -16,6 +18,7 @@ const App = () => {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
       await window.ethereum.enable();
+      await loadTetherContract();
     } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
     } else {
@@ -46,7 +49,6 @@ const App = () => {
   };
   const handleLoad = async () => {
     await handleCheckWeb3();
-    await loadTetherContract();
   };
   React.useEffect(() => {
     handleLoad();
